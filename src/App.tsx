3e1b0ffb7ab5ed610b2e6a9f9743ad7798b01f33
vishal0316 <vr987195@gmail.com>
2024-05-04 16:20:16 +0530

    Shadcn UI added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1>Welcome to elib dash</h1>
+      <Button variant={"default"}> Click Me </Button>
     </>
   );
 }
