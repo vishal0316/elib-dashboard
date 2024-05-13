889d7f3b5a11f95e0aa9c9097825aec8dfe66db0
vishal0316 <vr987195@gmail.com>
2024-05-13 16:15:42 +0530

    add tenstack

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,9 +9,21 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleLoginSubmit = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    console.log("data", { email, password });
+
+    // make server log
+  };
+
   return (
     <div>
       <div className="flex justify-center items-center h-screen bg-slate-200">
@@ -26,6 +38,7 @@ const LoginPage = () => {
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
+                ref={emailRef}
                 id="email"
                 type="email"
                 placeholder="m@example.com"
@@ -34,16 +47,18 @@ const LoginPage = () => {
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" required />
+              <Input ref={passwordRef} id="password" type="password" required />
             </div>
           </CardContent>
           <CardFooter>
             <div className="w-full">
-              <Button className="w-full">Sign in</Button>
+              <Button onClick={handleLoginSubmit} className="w-full">
+                Sign in
+              </Button>
 
               <div className="mt-4 text-center text-sm">
                 Don't have an account?{" "}
-                <Link to={"/register"} className="underline">
+                <Link to={"/auth/register"} className="underline">
                   Sign up
                 </Link>
               </div>
