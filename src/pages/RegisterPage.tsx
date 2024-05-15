e9fdf52a5cc8bb24d69b810ca66724299bee8cbd
vishal0316 <vr987195@gmail.com>
2024-05-15 13:11:25 +0530

    add register functionality

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -8,52 +8,96 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { register } from "@/http/api";
+import { useMutation } from "@tanstack/react-query";
+import { LoaderCircle } from "lucide-react";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      toast.success("Register successfully");
+      console.log("Login successful");
+      navigate("/dashboard/home");
+    },
+  });
+
+  const handleRegisterSubmit = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    const name = nameRef.current?.value;
+
+    if (!name || !email || !password) {
+      toast.warning("Please enter a valid email or password");
+      return;
+      // return alert("Please enter email and password");
+    }
+
+    mutation.mutate({ name, email, password });
+  };
   return (
-    <div>
-      <div className="flex justify-center items-center h-screen bg-slate-200">
-        <Card className="mx-auto max-w-sm">
-          <CardHeader>
-            <CardTitle className="text-xl">Sign Up</CardTitle>
-            <CardDescription>
-              Enter your information to create an account
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="first-name">Name</Label>
-                <Input id="first-name" placeholder="Jhon Wick" required />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" />
-              </div>
-              <Button type="submit" className="w-full">
-                Create an account
-              </Button>
+    <section className="flex justify-center items-center h-screen">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to create an account
+          </CardDescription>
+          {mutation.isError && (
+            <span className="text-red-500 text-sm">
+              {"Something went wrong"}
+            </span>
+          )}
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Input ref={nameRef} id="name" placeholder="Max" required />
             </div>
-            <div className="mt-4 text-center text-sm">
-              Already have an account?{" "}
-              <Link to={"/auth/login"} className="underline">
-                Sign in
-              </Link>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                ref={emailRef}
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
             </div>
-          </CardContent>
-        </Card>
-      </div>
-    </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input ref={passwordRef} id="password" type="password" />
+            </div>
+
+            <Button
+              onClick={handleRegisterSubmit}
+              className="w-full"
+              disabled={mutation.isPending}
+            >
+              {mutation.isPending && <LoaderCircle className="animate-spin" />}
+
+              <span className="ml-2">Create an account</span>
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link to={"/auth/login"} className="underline">
+              Sign in
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </section>
   );
 };
 
