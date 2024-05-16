bb0dfebdc3a2e01eba022c93f3c9c89725dd92c3
vishal0316 <vr987195@gmail.com>
2024-05-16 12:26:54 +0530

    persist access token i

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,6 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { login } from "@/http/api";
+import useTokenStore from "@/store";
 import { useMutation } from "@tanstack/react-query";
 // import { useMutation } from "@tanstack/react-query";
 import { useRef } from "react";
@@ -20,14 +21,15 @@ import { LoaderCircle } from "lucide-react";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const setToken = useTokenStore((state) => state.setToken);
 
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
   const mutation = useMutation({
     mutationFn: login,
-    onSuccess: () => {
-      console.log("success");
+    onSuccess: (response) => {
+      setToken(response.data.accessToken);
       navigate("/dashboard/home");
     },
   });
