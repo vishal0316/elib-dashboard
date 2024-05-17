f087e39c7f7e346a4dd06139c3fbc39283090637
vishal0316 <vr987195@gmail.com>
2024-05-17 13:14:26 +0530

    books table

--- src/http/api.ts
+++ src/http/api.ts
@@ -17,3 +17,5 @@ export const register = async (data: {
   email: string;
   password: string;
 }) => api.post("/api/users/register", data);
+
+export const getBooks = async () => api.get("/api/books");
