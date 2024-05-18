906af02b90e0c67cb30d8cadfc51be6a6ec25135
vishal0316 <vr987195@gmail.com>
2024-05-18 19:42:03 +0530

    create book

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,16 +1,24 @@
 import axios from "axios";
+import useTokenStore from "@/store";
 
 const api = axios.create({
-  // env
-  baseURL: "http://localhost:5513/",
+  // todo: move this value to env variable.
+  baseURL: "http://localhost:5513",
   headers: {
-    "content-type": "application/json",
+    "Content-Type": "application/json",
   },
 });
 
-export const login = async (data: { email: string; password: string }) => {
-  return api.post("/api/users/login", data);
-};
+api.interceptors.request.use((config) => {
+  const token = useTokenStore.getState().token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const login = async (data: { email: string; password: string }) =>
+  api.post("/api/users/login", data);
 
 export const register = async (data: {
   name: string;
@@ -19,3 +27,10 @@ export const register = async (data: {
 }) => api.post("/api/users/register", data);
 
 export const getBooks = async () => api.get("/api/books");
+
+export const createBook = async (data: FormData) =>
+  api.post("/api/books", data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
